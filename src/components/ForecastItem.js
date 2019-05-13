70d695d29e66abb3ea324fbb571fcf9c94c9861a
Nagy Robert <robert.cristian@me.com>
2019-05-13 21:54:33 +0300

    Small Visual Changes

--- src/components/ForecastItem.js
+++ src/components/ForecastItem.js
@@ -4,8 +4,8 @@ const ForecastItem = props => (
     <div className="col-4">
         <div className="forecast__item">
             <p className="forecast__timestamp">{props.dt}</p>
-            <p className="forecast__description">{props.description}</p>
-            <p className="forecast__temperature">{props.temperature}</p>
+            <p className="forecast__description text-capitalize">{props.description}</p>
+            <p className="forecast__temperature">Temp: {props.temperature}</p>
         </div>
     </div>
 );
